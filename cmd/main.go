68eaac76a1028d5b44cfd09216973d621e636c98
Carlos Cuba <ce.cuba@live.com>
2024-04-25 03:52:20 -0500

    Set configuration via .env and db connection structure

--- cmd/main.go
+++ cmd/main.go
@@ -1,13 +1,39 @@
 package main
 
 import (
+	"database/sql"
 	"github.com/charlieVader/ecom/cmd/api"
+	"github.com/charlieVader/ecom/config"
+	"github.com/charlieVader/ecom/db"
+	"github.com/go-sql-driver/mysql"
 	"log"
 )
 
 func main() {
-	server := api.NewAPIServer(":8080", nil)
+	database, err := db.NewMySQLStorage(mysql.Config{
+		User:                 config.Envs.DBUser,
+		Passwd:               config.Envs.DBPassword,
+		Addr:                 config.Envs.DBAddress,
+		DBName:               config.Envs.DBName,
+		Net:                  "tcp",
+		AllowNativePasswords: true,
+		ParseTime:            true,
+	})
+	if err != nil {
+		log.Fatal(err)
+	}
+	initStorage(database)
+
+	server := api.NewAPIServer(":8080", database)
 	if err := server.Run(); err != nil {
 		log.Fatal(err)
 	}
+}
+
+func initStorage(db *sql.DB) {
+	err := db.Ping()
+	if err != nil {
+		log.Fatal(err)
+	}
+	log.Println("DB: Successfully connected!")
 }
